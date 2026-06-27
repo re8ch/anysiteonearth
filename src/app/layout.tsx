01fabb86516b598734613e9b391bfe25139b7be0
Woomou <harry@re8ch.com>
2026-06-27 16:03:04 +0800

    Use current shared brand assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,10 +22,10 @@ export default function RootLayout({
     <html lang="zh-CN" data-lang="zh">
       <head>
         <meta name="brand-assets-base-url" content={brandAssetsOrigin} />
-        <link rel="stylesheet" href={`${brandAssetsOrigin}/dist/re8ch-navigator.css`} />
-        <link rel="stylesheet" href={`${brandAssetsOrigin}/dist/re8ch-footer.css`} />
-        <script src={`${brandAssetsOrigin}/dist/re8ch-navigator.js`} defer></script>
-        <script src={`${brandAssetsOrigin}/dist/re8ch-footer.js`} defer></script>
+        <link rel="stylesheet" href={`${brandAssetsOrigin}/dist/current/re8ch-navigator.css`} />
+        <link rel="stylesheet" href={`${brandAssetsOrigin}/dist/current/re8ch-footer.css`} />
+        <script src={`${brandAssetsOrigin}/dist/current/re8ch-navigator.js`} defer></script>
+        <script src={`${brandAssetsOrigin}/dist/current/re8ch-footer.js`} defer></script>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z0X8KS7NNX"></script>
         <script
           dangerouslySetInnerHTML={{
